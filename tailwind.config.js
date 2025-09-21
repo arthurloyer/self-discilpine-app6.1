2f9452cbd980e5a5647f9daeeec0476f355241f9
arthurloyer <arthurloyer22450@gmail.com>
2025-09-21 12:21:20 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        neon: { blue: "#38bdf8", violet: "#a855f7" },
-      },
-      boxShadow: {
-        holo: "0 0 18px rgba(99,102,241,.2), 0 0 42px rgba(56,189,248,.15)",
-      },
-    },
+        "neon-blue": "#38bdf8",
+        "neon-violet": "#a855f7"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
